1f3661c3ab32a9eb64875515f582efd9c7cc1561
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-05-12 23:09:58 +0330

    Update

--- run_detect.sh
+++ run_detect.sh
@@ -2,7 +2,7 @@ echo ----------------------------start detecting ------------------------------
 
 for((integer = 0; integer <= 24; integer ++))
 do
-  foo1="python univ_bd.py --model_dir model$integer"
+  foo1="python /kaggle/working/MM-BD/univ_bd.py --model_dir model$integer"
   $foo1
 done
 
